7dcabcf8825cc4a42d2411319e79ba84410dec61
trak-dev <yann.picaud@gmail.com>
2021-02-17 12:01:13 +0100

    messages nuls

--- app/src/main/java/com/example/pictochat/Chat.java
+++ app/src/main/java/com/example/pictochat/Chat.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Chat extends AppCompatActivity {
-
+    // test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
